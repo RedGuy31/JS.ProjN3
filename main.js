001032aae3634237cbc12c0bb37fbfd65f274ade
RedGuy <I.maisuradze31.19@gmail.com>
2021-08-17 15:14:42 +0400

    =)

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const current0EL = document.getElementById("current--0");
 const current1EL = document.getElementById("current--1");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
@@ -12,7 +14,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice
 btnRoll.addEventListener("click", function () {
@@ -22,7 +26,13 @@ btnRoll.addEventListener("click", function () {
 
   if (dice !== 1) {
     currentScore += dice;
-    current0EL.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
