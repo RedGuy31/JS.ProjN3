a8960b544759a19affd106c4f1d77a8494196875
RedGuy <I.maisuradze31.19@gmail.com>
2021-08-17 14:27:42 +0400

    dice

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+const current0EL = document.getElementById("current--0");
+const current1EL = document.getElementById("current--1");
+
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -8,3 +11,18 @@ const btnHold = document.querySelector(".btn--hold");
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+
+let currentScore = 0;
+
+// Rolling dice
+btnRoll.addEventListener("click", function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove("hidden");
+  diceEl.src = `./Dice/dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0EL.textContent = currentScore;
+  } else {
+  }
+});
