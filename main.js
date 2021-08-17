9899bf0b2a1dc2b63f0e7c3b3a5f1267cb09fa19
RedGuy <I.maisuradze31.19@gmail.com>
2021-08-17 20:09:26 +0400

    Done Let Roll = )

--- main.js
+++ main.js
@@ -10,14 +10,27 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+//starting point
+const init = function () {
+  const scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0EL.textContent = 0;
+  current1EL.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player1El.classList.remove("player-active");
+  player0El.classList.add("player--active");
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -66,14 +79,5 @@ btnHold.addEventListener("click", function () {
   }
 });
 
-//reset
-btnNew.addEventListener("click", function () {
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0EL.textContent = 0;
-  current1EL.textContent = 0;
-  player0El.classList.remove("player--winner");
-  player1El.classList.remove("player--winner");
-  player1El.classList.remove("player-active");
-  player0El.classList.add("player--active");
-});
+//reset vrbl
+btnNew.addEventListener("click", init);
